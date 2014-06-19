c199c8281c83292bf74aba4584a822b763d61ccb
Markus Bergholz <markuman@gmail.com>
2014-06-19 20:29:40 +0200

    add more input checks

--- web/gofs.php
+++ web/gofs.php
@@ -3,7 +3,7 @@
 <br />
 <h2>GNU Octave Function Search</h2>
 <br />
-<form method="post" action="fs.php">
+<form method="post" action="gofs.php">
 Function: <input type="text" name="function">
 <input class="button gray small" type="submit" name="Check" value=">> Search">
 </form>
@@ -16,7 +16,10 @@ $f = $_POST["function"];
 
 
 // functionality
+if (strlen($f) == 0) die('No Input was given');
 if (!preg_match('/^[a-zA-Z_0-9]+$/i',$f)) die('Illegal characters');
+if (strlen($f) > 64) die('Input is to long');
+
 	$q = "SELECT host, package FROM fs WHERE func='$f'";
 	$db = new SQLite3($dbfile);
 	$result = $db->query($q);
@@ -35,5 +38,7 @@ if (!preg_match('/^[a-zA-Z_0-9]+$/i',$f)) die('Illegal characters');
 	}
 	$db->close();
 ?>
+<br /> <br />
+<small><a href="https://github.com/octave-de/GOFS">Improve it!</a></small>
 
 
