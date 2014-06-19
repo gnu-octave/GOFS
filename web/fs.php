15b6697dbe8387e94cdd23f99c5630780d9dd6b6
Markus Bergholz <markuman@gmail.com>
2014-06-19 20:07:38 +0200

    add web search

---
+++ web/fs.php
@@ -0,0 +1,39 @@
+<html> 
+<link rel="stylesheet" type="text/css" href="main.css" />
+<br />
+<h2>GNU Octave Function Search</h2>
+<br />
+<form method="post" action="fs.php">
+Function: <input type="text" name="function">
+<input class="button gray small" type="submit" name="Check" value=">> Search">
+</form>
+<?php
+// options
+$dbfile = 'fs.sql';
+
+// inputs
+$f = $_POST["function"];
+
+
+// functionality
+if (!preg_match('/^[a-zA-Z_0-9]+$/i',$f)) die('Illegal characters');
+	$q = "SELECT host, package FROM fs WHERE func='$f'";
+	$db = new SQLite3($dbfile);
+	$result = $db->query($q);
+	if (!$result) {
+		echo "fuck this shit!<br >";
+	} else {
+		echo '<br /><ul>';
+		while ($record = $result->fetchArray()) {
+			if ( $record['host'] == "github" ) {
+				echo '<li><p> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="https://github.com/octave-de/octave-hub">' .$record['package'] .'</a></p></li>';
+			} elseif ( $record['host'] == "forge" ) {
+				echo '<li> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="http://octave.sf.net/'. $record['package'] .'/index.html">' .$record['package'] .'</a></p></li>';
+			}
+		}
+		echo '</ul>';
+	}
+	$db->close();
+?>
+
+
