e676bf18188fd076022b2ce4635dadad91b8922e
Markus Bergholz <markuman@gmail.com>
2014-06-22 10:40:25 +0200

    no one file + not found output

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 <br />
 <h2>GNU Octave Function Search</h2>
 <br />
-<form method="post" action="gofs.php">
+<form method="post" action="index.php">
 Function: <input type="text" name="function">
 <input class="button gray small" type="submit" name="Check" value=">> Search">
 </form>
@@ -16,7 +16,7 @@ $f = $_POST["function"];
 
 
 // functionality
-if (strlen($f) == 0) die('No Input was given');
+if (strlen($f) == 0) die('Database Version 2014.0615<br /><small><a href="https://github.com/octave-de/GOFS">Improve it!</a></small>');
 if (strlen($f) > 64) die('Input is to long');
 if (!preg_match('/^[a-zA-Z_0-9]+$/i',$f)) die('Illegal characters');
 
@@ -27,16 +27,26 @@ if (!preg_match('/^[a-zA-Z_0-9]+$/i',$f)) die('Illegal characters');
 		echo "fuck this shit!<br >";
 	} else {
 		echo '<br /><ul>';
-		while ($record = $result->fetchArray()) {
+		if ($record = $result->fetchArray()) {
 			if ( $record['host'] == "github" ) {
-				echo '<li><p> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="https://github.com/octave-de/octave-hub">' .$record['package'] .'</a></p></li>';
+				echo '<li><p> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="https://github.com/octave-de/octave-hub">'  .$record['package'] .'</a></p></li>';
 			} elseif ( $record['host'] == "forge" ) {
 				echo '<li> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="http://octave.sf.net/'. $record['package'] .'/index.html">' .$record['package'] .'</a></p></li>';
 			}
-		}
-		echo '</ul>';
+			while ($record = $result->fetchArray()) {
+				if ( $record['host'] == "github" ) {
+					echo '<li><p> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="https://github.com/octave-de/octave-hub">' .$record['package'] .'</a></p></li>';
+				} elseif ( $record['host'] == "forge" ) {
+					echo '<li> <b>'. $f .'()</b> is available in '. $record['host'] .' package <a href="http://octave.sf.net/'. $record['package'] .'/index.html">' .$record['package'] .'</a></p></li>';
+				}
+			}
+			echo '</ul>';
+		} else { echo 'Not found :(<br />Note: Octave core functions are not indexd.'; }
 	}
 	$db->close();
+
+
+
 ?>
 <br /> <br />
 <small><a href="https://github.com/octave-de/GOFS">Improve it!</a></small>
